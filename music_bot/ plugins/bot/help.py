bd15dd66f43c212aab6729de17226b4d44de6115
‹ 𝚲 𝚼 𝐈 › <164884298+Notookk@users.noreply.github.com>
2024-08-30 22:33:49 +0530

    Update help.py

--- music_bot/ plugins/bot/help.py
+++ music_bot/ plugins/bot/help.py
@@ -1,7 +1,15 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
+# Help command to show available bot commands
 @Client.on_message(filters.command("help") & filters.private)
 async def help(client: Client, message: Message):
-    help_text = "Here are the available commands:\n/help - Show this help message\n/play - Play a song\n/skip - Skip the current song\n/stop - Stop playing"
+    help_text = (
+        "Here are the available commands:\n"
+        "/help - Show this help message\n"
+        "/play <song_name> - Play a song\n"
+        "/skip - Skip the current song\n"
+        "/stop - Stop playing\n"
+        "/pause - Pause the music\n"
+    )
     await message.reply_text(help_text)
