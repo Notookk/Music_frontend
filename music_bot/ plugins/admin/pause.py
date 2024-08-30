7d72fe7e1fd6e622042176c16d91d01e4ca006ac
‹ 𝚲 𝚼 𝐈 › <164884298+Notookk@users.noreply.github.com>
2024-08-30 22:35:03 +0530

    Update pause.py

--- music_bot/ plugins/admin/pause.py
+++ music_bot/ plugins/admin/pause.py
@@ -1,6 +1,12 @@
 from pyrogram import Client, filters
+from pyrogram.types import Message
+from utils.permissions import admin_required
+from flask_socketio import emit
 
-@Client.on_message(filters.command('pause') & filters.user('YOUR_ADMIN_ID'))
-async def pause(client, message):
-    # Code to pause the music on the web
-    await message.reply_text("Music paused.")
+# Pause command to pause the music for admins
+@Client.on_message(filters.command("pause") & filters.group)
+@admin_required
+async def pause(client: Client, message: Message):
+    chat_id = message.chat.id
+    emit('pause', {'chat_id': chat_id}, namespace='/music')
+    await message.reply_text("Paused the music!")
