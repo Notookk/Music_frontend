081da20d4f51953be0c4b415c6822ec9ccb53021
‹ 𝚲 𝚼 𝐈 › <164884298+Notookk@users.noreply.github.com>
2024-08-30 22:34:33 +0530

    Update start.py

--- music_bot/ plugins/bot/start.py
+++ music_bot/ plugins/bot/start.py
@@ -1,5 +1,8 @@
 from pyrogram import Client, filters
+from pyrogram.types import Message
 
-@Client.on_message(filters.command('start') & filters.private)
-async def start(client, message):
-    await message.reply_text("Welcome to the Music Bot! Use /play <song name> to play music.")
+# Start command to welcome new users
+@Client.on_message(filters.command("start") & filters.private)
+async def start(client: Client, message: Message):
+    start_text = "Welcome to the bot! Use /help to see available commands."
+    await message.reply_text(start_text)
